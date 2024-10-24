095532a6cca7bde4140a944f07db569faa2b4d1f
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-10-24 13:28:07 -0600

    Create Menu Bars and Menu UI

--- SwiftUITinderTrip/ContentView.swift
+++ SwiftUITinderTrip/ContentView.swift
@@ -10,15 +10,63 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TopBarMenu()
+            CardView(image: "yosemite-usa", title: "Yosmite, USA")
+            Spacer(minLength: 20)
+            BottomBarMenu()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+#Preview("TopBarMenu"){
+    TopBarMenu()
+}
+#Preview("BottomBarMenu"){
+    BottomBarMenu()
+}
+
+
+struct TopBarMenu: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "line.horizontal.3")
+                .font(.system(size: 30))
+            Spacer()
+            Image(systemName: "mappin.and.ellipse")
+                .font(.system(size: 35))
+            Spacer()
+            Image(systemName: "heart.circle.fill")
+                .font(.system(size: 30))
+        }
+        .padding()
+    }
+}
+
+struct BottomBarMenu: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "xmark")
+                .font(.system(size: 30))
+                .foregroundColor(.black)
+            Button {
+                //Book the trip
+            } label: {
+                Text("BOOK IT NOW")
+                    .font(.system(.subheadline, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 35)
+                    .padding(.vertical, 15)
+                    .background(.black)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal, 20)
+            Image(systemName: "heart")
+                .font(.system(size: 30))
+                .foregroundStyle(.black)
+        }
+    }
 }
