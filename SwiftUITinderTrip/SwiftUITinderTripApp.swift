e67bbbdf438f9c62444e680c00dc5472f4fbe729
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-10-23 18:07:35 -0600

    Create CardView static

---
+++ SwiftUITinderTrip/SwiftUITinderTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITinderTripApp.swift
+//  SwiftUITinderTrip
+//
+//  Created by Simon Ng on 17/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITinderTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
