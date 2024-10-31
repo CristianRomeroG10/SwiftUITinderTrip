4df64240662dba77138f787f367848ccf14cc90f
Cristian Romero <cristian.romerogarcia10@gmail.com>
2024-10-30 19:54:12 -0600

    implement swiping Motion

--- SwiftUITinderTrip/ContentView.swift
+++ SwiftUITinderTrip/ContentView.swift
@@ -9,6 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     
+    enum DragState {
+        case inactive
+        case pressing
+        case dragging(translation: CGSize)
+        
+        var translation: CGSize {
+            switch self{
+            case .inactive, .pressing:
+                return .zero
+            case .dragging(let traslation):
+                return traslation
+            }
+        }
+        
+        var isDragging: Bool {
+            switch self{
+            case .dragging:
+                return true
+            case .inactive, .pressing:
+                return false
+            }
+        }
+        
+        var isPressing: Bool {
+            switch self{
+            case .pressing, .dragging:
+                return true
+            case .inactive:
+                return false
+            }
+        }
+    }
+    
+    @GestureState private var dragState = DragState.inactive
+    
     private func isTopCard(cardView: CardView)->Bool{
         guard let index = cardViews.firstIndex(where: { $0.id == cardView.id }) else {
             return false
@@ -30,10 +65,29 @@ struct ContentView: View {
                 ForEach(cardViews){ cardView in
                     cardView
                         .zIndex(self.isTopCard(cardView: cardView) ? 1 : 0)
+                        .offset(x: self.isTopCard(cardView: cardView) ? self.dragState.translation.width : 0, y:self.isTopCard(cardView: cardView) ? self.dragState.translation.height : 0)
+                        .scaleEffect(self.dragState.isDragging && self.isTopCard(cardView: cardView) ? 0.95 : 1.0)
+                        .rotationEffect(Angle(degrees:self.isTopCard(cardView: cardView) ? Double(self.dragState.translation.width / 10):0))
+                        .animation(.interpolatingSpring(stiffness: 180, damping: 100), value: self.dragState.translation)
+                        .gesture(LongPressGesture(minimumDuration: 0.01)
+                            .sequenced(before: DragGesture())
+                            .updating(self.$dragState, body: { (value, state,transaction) in
+                                switch value {
+                                case .first(true):
+                                    state = .pressing
+                                case .second(true, let drag):
+                                    state = .dragging(translation: drag?.translation ?? .zero)
+                                default:
+                                    break
+                                }
+                            })
+                        )
                 }
             }
             Spacer(minLength: 20)
             BottomBarMenu()
+                .opacity(dragState.isDragging ? 0.0 : 1.0)
+                .animation(.default, value: dragState.isDragging)
         }
     }
 }
